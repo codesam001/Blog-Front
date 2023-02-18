f57e287a1105fd86f6ec70222dbb4c231a1d51a6
codesam001 <105776000+codesam001@users.noreply.github.com>
2023-02-18 08:50:46 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,21 @@
 
 import axios from "axios"
 
-export const axiosInstance = axios.create({
-    baseURL : "https://blog-backend-e3mw.onrender.com/api/"
-})
+// export const axiosInstance = axios.create({
+//     baseURL : "https://blog-backend-e3mw.onrender.com/api/"
+// })
 
 
 
+const axiosInstance = axios.create({
+    baseURL: 'https://blog-backend-e3mw.onrender.com/api/',
+   
+  });
+
+
+  export default axiosInstance;
+
+
+
+
+  
